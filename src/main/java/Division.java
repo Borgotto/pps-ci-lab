b29ba742824b2e83db913d466c37c0e59355285f
Cristina Zoccola <cristina.zoccola@studio.unibo.it>
2026-04-20 11:42:31 +0200

    Added javadoc

--- src/main/java/Division.java
+++ src/main/java/Division.java
@@ -1,12 +1,24 @@
+/**
+ * Simple class to do a division between two numbers
+ */
 public class Division {
     private float firstNumber;
     private float secondNumber;
 
+    /**
+     *
+     * @param firstNumber the dividend of the division
+     * @param secondNumber the divisor of the division
+     */
     public Division(float firstNumber, float secondNumber) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
     }
 
+    /**
+     *
+     * @return the quotient of the division
+     */
     public float doDivision() {
         if (secondNumber != 0) {
             return firstNumber / secondNumber;
