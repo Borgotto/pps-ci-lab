55360161b3e256f875826b344498a63510cfd9a1
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-04-25 12:29:19 +0200

    add tests, update ci script

---
+++ src/test/java/SubtractionUtilTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * A Java test class for {@link SubtractionUtil}.
+ */
+public class SubtractionUtilTest {
+
+    /**
+     * Tests the subtraction between two numbers.
+     */
+    @Test
+    public void testSubtraction() {
+        final var a = 5;
+        final var b = 3;
+        final var result = SubtractionUtil.subtract(a, b);
+        final var expectedResult = 2;
+        assertEquals(expectedResult, result);
+    }
+
+}
