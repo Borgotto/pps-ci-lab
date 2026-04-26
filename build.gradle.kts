6e6ffb0914ce0b630729c23bcce7f81a93a89ce5
Emanuele Borghini <39804469+Borgotto@users.noreply.github.com>
2026-04-26 16:12:50 +0200

    Add junit to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,26 @@
 plugins {
     java
+    scala
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Java
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Scala
+    val scalaVersion = "3.3.7"
+    val (scalaMinor, _) = requireNotNull(Regex("^(\\d+)(\\.\\d+)(\\.\\d+)?$").matchEntire(scalaVersion)).destructured
+    implementation("org.scala-lang:scala3-library_3:${scalaVersion}")
+}
+
+// Enables JUnit Platform (needed for JUnit 5)
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
